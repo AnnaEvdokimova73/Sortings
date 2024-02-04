6eb92eba8fd1885b62b7c3b191b3b3c4322499ed
Anna Evdokimova <evdockimova.annie@yandex.ru>
2024-02-04 13:03:49 +0300

    Added quick sort and counting sort

---
+++ countingSort/countingSort.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+void count_sort(int* arr, int size)
+{
+    int* counts = new int[25];
+
+    for (int i = 0; i < 25; ++i)
+    {
+        counts[i] = 0;
+    }
+
+    for (int i = 0; i < size; ++i)
+    {
+        counts[arr[i]] += 1;
+    }
+
+    int c = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        while (counts[c] == 0)
+        {
+            ++c;
+        }
+        arr[i] = c;
+        counts[c] -= 1;
+    }
+
+    delete[] counts;
+}
+
+void printArrays(int* arr, int size)
+{
+    std::cout << "Исходный массив: ";
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    count_sort(arr, size);
+
+    std::cout << "Отсортированный массив: ";
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    const int size = 30;
+    int arr[size] = { 19, 14, 22, 22, 17, 22, 13, 21, 20, 24, 18, 10, 17, 16, 17, 20, 22, 11, 20, 16, 14, 13, 10, 22, 18, 14, 16, 24, 19, 17 };
+    const int size1 = 40;
+    int arr1[size1] = { 16, 17, 14, 20, 22, 20, 17, 22, 16, 19, 23, 24, 20, 22, 21, 18, 14, 16, 17, 21, 10, 11, 19, 23, 11, 11, 17, 17, 11, 21, 17, 11, 17, 16, 12, 11, 16, 22, 23, 16 };
+    const int size2 = 46;
+    int arr2[size2] = { 21, 15, 19, 18, 23, 12, 18, 18, 19, 23, 12, 20, 15, 22, 21, 18, 19, 20, 12, 16, 20, 14, 17, 13, 10, 23, 19, 14, 10, 22, 19, 12, 24, 23, 22, 15, 13, 22, 18, 18, 11, 23, 24, 17, 10, 16 };
+    printArrays(arr, size);
+    printArrays(arr1, size1);
+    printArrays(arr2, size2);
+}
---
+++ quickSort/quickSort.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+
+void quickSort(int* arr, int start, int end)
+{
+    if (start >= end)
+    {
+        return;
+    }
+
+    int left = start;
+    int right = end;
+    int pi = (left + right + 1) / 2; //индекс пивота
+    int pivot = arr[pi];
+
+    while (left < right)
+    {
+        while (arr[left] < pivot)
+        {
+            ++left;
+        }
+        while (arr[right] > pivot)
+        {
+            --right;
+        }
+        if (left <= right)
+        {
+            std::swap(arr[left], arr[right]);
+            ++left;
+            --right;
+        }
+    }
+
+    quickSort(arr, start, right);
+    quickSort(arr, left, end);
+}
+
+void quick_sort(int* arr, int size)
+{
+    if (size < 2)
+    {
+        return;
+    }
+
+    int left = 0;
+    int right = size - 1;
+
+    quickSort(arr, left, right);
+}
+
+void printArrays(int* arr, int size)
+{
+    std::cout << "Исходный массив: ";
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    quick_sort(arr, size);
+
+    std::cout << "Отсортированный массив: ";
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    const int size = 10;
+    int arr[size] = { 3, 43, 38, 29, 18, 72, 57, 61, 2, 33 };
+    const int size1 = 15;
+    int arr1[size1] = { 88, 91, 87, 59, 53, 49, 29, 16, 4, 27, 28, 89, 2, 25, 74 };
+    const int size2 = 18;
+    int arr2[size2] = { 24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
+    printArrays(arr, size);
+    printArrays(arr1, size1);
+    printArrays(arr2, size2);
+}
